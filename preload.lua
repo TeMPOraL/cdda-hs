f92fc2c5f0e055aabf4047be8538ea48e595f36e
Jacek TeMPOraL Złydach <temporal.pl@gmail.com>
2017-10-18 02:50:56 +0200

    Stubbing lua iuse actions for SDR.

---
+++ preload.lua
@@ -0,0 +1,16 @@
+function iuse_hs_sdr_on(item, active)
+   -- TODO
+   -- currently just debugging
+   game.add_msg("You turn on your software-defined radio.")
+   return 0 -- 0 charges used; I think it's ok, right?
+end
+
+function iuse_hs_sdr_off(item, active)
+   -- TODO
+   -- currently just debugging
+   game.add_msg("You turn off your software-defined radio.")
+   return 0 -- 0 charges used; I think it's ok, right?
+end
+
+game.register_iuse("IUSE_LUA_HS_SDR_ON", iuse_hs_sdr_on)
+game.register_iuse("IUSE_LUA_HS_SDR_OFF", iuse_hs_sdr_off)
